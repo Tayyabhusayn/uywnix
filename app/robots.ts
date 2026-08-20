46ccca5cbedc35f4fa7fa60065edd14545b9e4da
Tayyabhusayn <razintayyabr@gmail.com>
2026-08-20 06:29:30 +0000

    seo: robots.ts with AI bot + Baidu + Sogou rules

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,49 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/private/',
+      },
+      {
+        userAgent: 'GPTBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'ClaudeBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'PerplexityBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Google-Extended',
+        allow: '/',
+      },
+      {
+        userAgent: 'Applebot-Extended',
+        allow: '/',
+      },
+      {
+        userAgent: 'cohere-ai',
+        allow: '/',
+      },
+      {
+        userAgent: 'bingbot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Baiduspider',
+        allow: '/',
+      },
+      {
+        userAgent: 'Sogou web spider',
+        allow: '/',
+      },
+    ],
     sitemap: 'https://www.uywnix.com/sitemap.xml',
   };
 }
